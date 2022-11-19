72d990b95bdac9d030842ac59e7d8f4a10aad93e
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-11-19 12:57:49 -0300

    Configure global error handler

--- src/app.js
+++ src/app.js
@@ -23,3 +23,18 @@ App.use(express.json());
 App.use('/v1', routes);
 
 App.use(errors());
+App.use(async (error, _, response, next) => {
+  if (isBoom(error)) {
+    const { statusCode, payload } = error.output;
+
+    return response.status(statusCode).json({
+      ...payload,
+      ...error.data,
+      docs: process.env.DOCS_URL,
+    });
+  }
+
+  return next(error);
+});
+
+export default App;

a46f0b3a18fab6d3366311daf8af0aba0798bcbb
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-02 00:14:13 -0300

    Add necessary repository methods

--- src/repositories/DriverRepository.js
+++ src/repositories/DriverRepository.js
@@ -1,6 +1,41 @@
 import Driver from '../models/Driver';
 
 class DriverRepository {
+  async find(conditions, { page, limit }) {
+    return Driver.find(conditions)
+      .limit(limit)
+      .skip((page - 1) * limit);
+  }
+
+  async count({ conditions }) {
+    return Driver.countDocuments(conditions);
+  }
+
+  async findById(id) {
+    return Driver.findById(id);
+  }
+
+  async create({
+    cpf,
+    name,
+    phone,
+    birthday,
+    gender,
+    cnh_number,
+    cnh_type,
+    vehicle_id,
+  }) {
+    return Driver.create({
+      cpf,
+      name,
+      phone,
+      birthday,
+      gender,
+      cnh_number,
+      cnh_type,
+      vehicle_id,
+    });
+  }
 }
 
 export default DriverRepository;

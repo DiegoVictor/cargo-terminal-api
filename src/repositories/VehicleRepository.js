aed9c1b8fe1fc7b9ea8bd4d65be937c5de47dffa
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-02 00:13:32 -0300

    Create required methods

--- src/repositories/VehicleRepository.js
+++ src/repositories/VehicleRepository.js
@@ -1,6 +1,23 @@
 import Vehicle from '../models/Vehicle';
 
 class VehicleRepository {
+  async find({ page, limit }) {
+    return Vehicle.find()
+      .limit(limit)
+      .skip((page - 1) * limit);
+  }
+
+  async count() {
+    return Vehicle.countDocuments();
+  }
+
+  async findById(id) {
+    return Vehicle.findById(id);
+  }
+
+  async create({ type, model }) {
+    return Vehicle.create({ type, model });
+  }
 }
 
 export default VehicleRepository;

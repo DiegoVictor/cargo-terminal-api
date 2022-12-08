cb5a2a8f465b1bd56923f5da353feffd639f92dc
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-08 08:23:03 -0300

    Create action to store a new driver

--- src/controllers/DriverController.js
+++ src/controllers/DriverController.js
@@ -1,6 +1,41 @@
+import StoreDriverService from '../services/StoreDriverService';
 import UpdateDriverService from '../services/UpdateDriverService';
 import getRepositories from '../utils/getRepositories';
 class DriverController {
+
+  async store(request, response) {
+    const {
+      cpf,
+      name,
+      phone,
+      birthday,
+      gender,
+      cnh_number,
+      cnh_type,
+      vehicle_id,
+    } = request.body;
+
+    const [driverRepository, vehicleRepository] = getRepositories([
+      'driver',
+      'vehicle',
+    ]);
+    const storeDriverService = new StoreDriverService(
+      driverRepository,
+      vehicleRepository
+    );
+    const driver = await storeDriverService.run({
+      cpf,
+      name,
+      phone,
+      birthday,
+      gender,
+      cnh_number,
+      cnh_type,
+      vehicle_id,
+    });
+
+    return response.status(201).json(driver);
+  }
 
   async update(request, response) {
     const { id } = request.params;

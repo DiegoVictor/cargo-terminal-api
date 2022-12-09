b3bac32c59e35c415eb4b2cdb818fec3002d820c
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-09 00:49:21 -0300

    Get drivers

--- src/controllers/DriverController.js
+++ src/controllers/DriverController.js
@@ -1,7 +1,33 @@
+import FindDriversService from '../services/FindDriversService';
 import StoreDriverService from '../services/StoreDriverService';
 import UpdateDriverService from '../services/UpdateDriverService';
 import getRepositories from '../utils/getRepositories';
+import paginationLinks from '../utils/paginationLinks';
+
 class DriverController {
+  async index(request, response) {
+    const { currentUrl } = request;
+    const { active, vehicle, page = 1 } = request.query;
+    const limit = 20;
+
+    const [driverRepository] = getRepositories('driver');
+    const findDriversService = new FindDriversService(driverRepository);
+    const { drivers, count } = await findDriversService.run({
+      active,
+      vehicle,
+      page,
+      limit,
+    });
+
+    response.header('X-Total-Count', count);
+
+    const pagesTotal = Math.ceil(count / limit);
+    if (pagesTotal > 1) {
+      response.links(paginationLinks(page, pagesTotal, currentUrl));
+    }
+
+    return response.json(drivers);
+  }
 
   async store(request, response) {
     const {

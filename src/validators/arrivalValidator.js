a6d3eb88acde7fabfd058b0eb087f0bd95fddd24
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-11 14:23:48 -0300

    Validate arrival update payload

--- src/validators/arrivalValidator.js
+++ src/validators/arrivalValidator.js
@@ -18,3 +18,23 @@ export const store = celebrate({
       .required(),
   }),
 });
+
+export const update = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    vehicle_id: Joi.string().optional(),
+    driver_id: Joi.string().optional(),
+    filled: Joi.boolean().optional(),
+    origin: Joi.object()
+      .keys({
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+      })
+      .optional(),
+    destination: Joi.object()
+      .keys({
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+      })
+      .optional(),
+  }),
+});

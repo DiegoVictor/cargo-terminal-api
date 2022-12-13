64a2fdc10e111ac46ed93cbb0b2d3d0aa0cfa978
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-13 18:04:02 -0300

    Create vehicle router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import vehicles from './vehicles';
 import arrivals from './arrivals';
 import travels from './travels';
 
 const app = Router();
 
+app.use('/vehicles', vehicles);
 app.use('/arrivals', arrivals);
 app.use('/travels', travels);
 

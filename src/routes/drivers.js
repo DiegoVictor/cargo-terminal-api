75bccdebc4aa76f415e40e790467560d56c91a92
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-13 18:04:43 -0300

    Add driver endpoints

--- src/routes/drivers.js
+++ src/routes/drivers.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import DriverController from '../controllers/DriverController';
+import idValidator from '../validators/idValidator';
+import * as driverValidator from '../validators/driverValidator';
+import activeVehiclePageValidator from '../validators/activeVehiclePageValidator';
+
 const app = Router();
+const driverController = new DriverController();
+
+app.get('/', activeVehiclePageValidator, driverController.index);
+app.post('/', driverValidator.store, driverController.store);
+app.put('/:id', idValidator, driverValidator.update, driverController.update);
 
 export default app;

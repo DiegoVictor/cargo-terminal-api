35d2200af040dbe6e7f4769074063ed851925f1a
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-13 18:04:10 -0300

    Add vehicle endpoints

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
 
+import VehicleController from '../controllers/VehicleController';
+
+import idValidator from '../validators/idValidator';
+import pageValidator from '../validators/pageValidator';
+import vehicleValidator from '../validators/vehicleValidator';
+
 const app = Router();
+const vehicleController = new VehicleController();
+
+app.get('/', pageValidator, vehicleController.index);
+app.post('/', vehicleValidator, vehicleController.store);
+app.put('/:id', idValidator, vehicleValidator, vehicleController.update);
 
 export default app;

9e2774c0ccc1c6aa681248c231298f131bd6c3b6
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-13 18:03:28 -0300

    Configure arrival routes

--- src/routes/arrivals.js
+++ src/routes/arrivals.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import ArrivalController from '../controllers/ArrivalController';
+import idValidator from '../validators/idValidator';
+import * as arrivalValidator from '../validators/arrivalValidator';
+import filledDateStartDateEndPageValidator from '../validators/filledDateStartDateEndPageValidator';
+
 const app = Router();
+const arrivalController = new ArrivalController();
+
+app.get('/', filledDateStartDateEndPageValidator, arrivalController.index);
+app.post('/', arrivalValidator.store, arrivalController.store);
+app.put('/:id', idValidator, arrivalValidator.update, arrivalController.update);
 
 export default app;

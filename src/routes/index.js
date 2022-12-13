3b55fa879953bef07d761da0190a1192e47de685
DiegoVictor <diegovictorgonzaga@gmail.com>
2022-12-13 18:04:34 -0300

    Create driver router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import drivers from './drivers';
 import vehicles from './vehicles';
 import arrivals from './arrivals';
 import travels from './travels';
 
 const app = Router();
 
+app.use('/drivers', drivers);
 app.use('/vehicles', vehicles);
 app.use('/arrivals', arrivals);
 app.use('/travels', travels);

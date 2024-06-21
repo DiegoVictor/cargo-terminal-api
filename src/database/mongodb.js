e67db980910472019bb70e6134e1679b8c1c7d14
DiegoVictor <diegovictorgonzaga@gmail.com>
2024-06-21 10:50:53 -0300

    Remove deprecated mongoose settings

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,11 +1,7 @@
 import mongoose from 'mongoose';
 
 const connection = mongoose.connect(
-  `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+  `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
 );
 
 export default connection;
